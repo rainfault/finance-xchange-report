60a34add694dd85cdbbb2dc8543a4301179e17eb
Zakhar <99883153+rainfault@users.noreply.github.com>
2024-03-22 15:02:45 +0300

    Delete config.py

--- config.py
+++
@@ -1,2 +0,0 @@
-USD_TO_RUB_URL = "http://iss.moex.com/iss/statistics/engines/futures/markets/indicativerates/securities/USD/RUB.json?from=2024-02-01&till=2024-02-29&iss.meta=off"
-JPY_TO_RUB_URL = "http://iss.moex.com/iss/statistics/engines/futures/markets/indicativerates/securities/JPY/RUB.json?from=2024-02-01&till=2024-02-29&iss.meta=off"
